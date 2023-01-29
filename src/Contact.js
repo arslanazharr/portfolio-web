800276c7d56e8e2d09b970f9750c7866408b7a85
Arslan <arslanazhar00@gmail.com>
2023-01-29 23:47:42 +0500

    cv updated

--- src/Contact.js
+++ src/Contact.js
@@ -18,7 +18,7 @@ const Contact = () => {
           Email
         </a>{" "}
         or hit me up via{" "}
-        <a href="https://www.linkedin.com/in/arslanazharr/" target="new">
+        <a href="" target="new">
           LinkedIn
         </a>
         . You can also find a small collection of my previous work on{" "}
